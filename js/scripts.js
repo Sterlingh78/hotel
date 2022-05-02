94c6dd473fa65e2a59cdb6118704fecfa18380e5
sterlingh78 <sterlingh78@yahoo.com>
2022-05-02 14:35:28 -0600

    json

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
-// what is the path to the JSON file?
-const jsonURL = "https://sterlingh78.github.io/hotel/js/hoteldata.json"
-
 //Go fetch it and then wait for a response.
-fetch(jsonURL)
+fetch('https://sterlingh78.github.io/hotel/js/hoteldata.json')
   .then((response) => response.json())
   .then((myList) => {
     //Once it comes back, display it to the console.
